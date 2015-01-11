d4de7cfe5b475a189a223e1c59e6116f19f1b341
Thomas Barembaum <thomas.barembaum@gmail.com>
2015-01-11 11:19:03 -0500

    GemModel

--- examples/i-am-bejeweled/src/models/GemModel.cpp
+++ examples/i-am-bejeweled/src/models/GemModel.cpp
@@ -1,1 +1,19 @@
 #include "GemModel.h"
+#include <iostream>
+
+using namespace std;
+
+void GemModel::Register(const char* textureId)
+{
+	m_List.push_back(textureId);
+}
+
+void GemModel::GetListCopy(vector<const char*>& origin, vector<const char*>& target)
+{
+	target.clear();
+	int l = origin.size();
+	for (int i = 0 ; i < l ; i++)
+	{
+		target.push_back(origin[i]);
+	}
+}
--- examples/i-am-bejeweled/src/models/GemModel.h
+++ examples/i-am-bejeweled/src/models/GemModel.h
@@ -1,11 +1,24 @@
 #ifndef _GEMMODEL_H_
 #define _GEMMODEL_H_
 
+#include <vector>
+
+using namespace std;
+
 class GemModel
 {
+private:
+	vector<const char*>		m_List;
+
+public:
+	vector<const char*>*	GetList()		{ return &m_List; }
+	
 public:
 	GemModel() {}
 	~GemModel() {}
+
+	void	Register(const char* textureId);
+	void	GetListCopy(vector<const char*>& origin, vector<const char*>& target);
 };
 
 #endif
